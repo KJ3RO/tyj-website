efcdfeaf8276ab950a5c487e76af457ad8af4ff6
KJ3RO <doyeonk0520@gmail.com>
2024-06-09 15:52:08 +0900

    completed basic structure

---
+++ app/components/Footer/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="text-center">© 2024 Thank You Jeju. All rights reserved.</div>;
+};
+
+export default Footer;
---
+++ app/components/issues/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EnvironmentalIssuesPage = () => {
+  return <div>EnvironmentalIssuesPage</div>;
+};
+
+export default EnvironmentalIssuesPage;
