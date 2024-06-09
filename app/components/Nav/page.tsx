6cf87ab5a045343dbaf219183cc9f1ea33d271b0
KJ3RO <doyeonk0520@gmail.com>
2024-06-09 22:31:27 +0900

    added pictures & details

--- app/components/Nav/page.tsx
+++ app/components/Nav/page.tsx
@@ -1,20 +1,12 @@
 "use client";
-import React, { useState } from "react";
-import { Menu } from "../../../components/ui/navbar-menu";
-import { cn } from "@/utils/cn";
 import Link from "next/link";
 
-const Navbar = ({ className }: { className?: string }) => {
-  const [active, setActive] = useState<string | null>(null);
+export default function Navbar() {
   return (
     <div className="fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 drop-shadow-lg">
-      <Menu setActive={setActive}>
-        <Link href="/">Home</Link>
-        <Link href="/components/issues">Research</Link>
-        <Link href="/components/gallery">Gallery</Link>
-      </Menu>
+      <Link href="/">Home</Link>
+      <Link href="/components/issues">Research</Link>
+      <Link href="/components/gallery">Gallery</Link>
     </div>
   );
-};
-
-export default Navbar;
+}
